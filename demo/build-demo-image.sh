0b3cb3abbe7552fa9aaf934d1e10117e8d6a8dbc
szogi <szogibalu@gmail.com>
2015-10-03 17:56:45 +0200

    H3-Fixed Docker build

---
+++ demo/build-demo-image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if !(which mvn > /dev/null;) then
+    echo Maven is not installed.
+    exit 1
+fi
+
+if !(which docker > /dev/null;) then
+    echo Docker is not installed.
+    exit 1
+fi
+
+if (mvn clean package docker:build); then
+  echo "Demo App is built.."
+else
+  echo "Error at building Demo App..."
+  exit 1
+fi
